d00857d7f1428a79006e8d42d9ab5d8af721fba6
TomasGalindo <tgalindoencinas@yahoo.com>
2017-04-24 13:54:02 +0200

    proyecto Django

---
+++ myproject/urls.py
@@ -0,0 +1,30 @@
+"""myproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from cms_put import views
+from django.contrib.auth.views import login, logout
+from django.views.static import serve
+from myproject import settings
+
+urlpatterns = [
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_URL + '/hifi_news/'}),
+    url(r'^$', views.writeBase, name="Listado de las paginas"),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^logout', logout, {'next_page': '/'}),
+    url(r'^login', login, {'template_name': 'registration/login.html'}),
+
+    url(r'^(.*)$', views.pagina, name="recurso a traves de id")
+]
